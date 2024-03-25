febe9c21c10b5deac30b1d1d369ddf15254a14d4
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-03-25 15:53:03 +0000

    created random number for further use

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function runGame() {
 
+    // create random number between 1 and 6
+    let num1 = Math.ceil(Math.random() * 6);
+    // console.log for functionality test
+    console.log(num1);
+
+    // make number show on page for further testing
+    document.getElementById('number').textContent = num1;
+
 };
 
 function winningStreak() {
